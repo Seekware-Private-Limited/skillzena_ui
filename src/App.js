cc26abdcaf906badda570c791a1935a885ef33c5
KapilMishra12 <131173807+KapilMishra12@users.noreply.github.com>
2023-09-21 11:39:51 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -7,6 +7,7 @@ import Home from "./pages/home";
 import HowToHire from "./pages/how-to-hire";
 import Layout from "./Layout";
 import HowToFindWork from "./pages/how-to-find-work";
+import Srevices from "./pages/services";
 
 
 
@@ -22,6 +23,7 @@ function App() {
           <Route path="/" element={<Layout />}>
           <Route path="why-skillzena/how-to-hire" element={<HowToHire />} />
           <Route path="why-skillzena/how-to-find-work" element={<HowToFindWork />} />
+          <Route path="services" element={<Srevices />} />
 
           {/* <Route path="/blogs" element={<Blogs />} /> */}
           </Route>
